954f3ba48ca710b10d649d3fc03a2722bdccc87e
Biraj Mainali <birajmainali10@gmail.com>
2024-12-19 18:53:59 +0545

    init

---
+++ OllamaGate/Program.cs
@@ -0,0 +1,32 @@
+using Scalar.AspNetCore;
+
+namespace OllamaGate;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddHealthChecks();
+
+        builder.Services.AddReverseProxy()
+            .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        app.MapOpenApi();
+        app.MapScalarApiReference();
+
+        app.UseHttpsRedirection();
+
+        app.MapReverseProxy();
+
+
+        app.MapHealthChecks("/health");
+        app.Run();
+    }
+}
